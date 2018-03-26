906d4a55a80fed2ae6912d36ecf61e80d6a5a747
Aidanurp <14maret1998@gmail.com>
2018-03-27 06:31:15 +0800

    objek 3

--- src/latihan01/motoraksi.java
+++ src/latihan01/motoraksi.java
@@ -27,6 +27,12 @@ public class motoraksi {
         b.harga_motor="25000000";
         b.warna_motor="red";
         
+        c.jenis_motor="scooter";
+        c.thn_produksi="2017";
+        c.jenis_transmisi"matic";
+        c.harga_motor="14000000";
+        c.warna_motor="white";
+        
         
         
         
