27eeb2a9b7b2c54c6c1f4200041aabaac0a63437
Aidanurp <14maret1998@gmail.com>
2018-03-27 06:24:54 +0800

    objek-objek

---
+++ src/latihan01/motoraksi.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan01;
+
+/**
+ *
+ * @author aidanurp
+ */
+public class motoraksi {
+    private static void main(string[] args) {
+        motor a = new motor();
+        motor b = new motor();
+        motor c = new motor();
+        
+        
+    }
+    
+}
